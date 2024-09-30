d24762f1e5f8275d84cfd13dcdd714352ce6a18f
Yi Ding <142643338+DripNowhy@users.noreply.github.com>
2024-09-30 21:48:56 +0800

    Delete script directory

--- script/debug.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Set CUDA device to GPU 5
-# export CUDA_LAUNCH_BLOCKING=1
-export CUDA_VISIBLE_DEVICES=4,5,6
-
-# Run the Python script with debugpy for remote debugging
-python -m debugpy --listen 0.0.0.0:9501 --wait-for-client text_generation.py
